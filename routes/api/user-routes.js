c7a17fb410734166c6e1945b44129900ef35b5c8
willc <willcrain1@gmail.com>
2022-11-06 12:32:21 -0500

    Created login page

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,17 +1,5 @@
 const router = require('express').Router();
 
-router.get('/login', async (req, res) => {
-    try {
-        if(req.session.loggedIn) {
-            
-            return;
-        }
 
-        res.render('login', {});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
 
 module.exports = router;
