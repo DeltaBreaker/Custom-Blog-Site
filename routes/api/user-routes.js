a2751ac7167e444822d002008bb69db3075a2afb
willc <willcrain1@gmail.com>
2022-11-06 13:32:20 -0500

    Added ability to sign up and log out

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,42 @@
 const router = require('express').Router();
+const { User } = require('../../sql/models');
 
+router.post("/", async (req, res) => {
+    try {
+        let response = await User.create(req.body);
 
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json({ success: true });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err, success: false });
+    }
+});
+
+router.get("/logout", async (req, res) => {
+    try {
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json({ success: true });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err, success: false });
+    }
+});
+
+router.get("/logout", async (req, res) => {
+    try {
+        req.session.save(() => {
+            req.session.loggedIn = false;
+            res.status(200).json({ success: true });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err, success: false });
+    }
+});
 
 module.exports = router;
