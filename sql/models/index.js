a7e5038a1e35932caa75ce16383bf425fb70401a
willc <willcrain1@gmail.com>
2022-11-07 15:46:05 -0500

    Added Comment model and relationships

--- sql/models/index.js
+++ sql/models/index.js
@@ -1,10 +1,11 @@
 const User = require('./User.js');
 const Post = require('./Post.js');
+const Comment = require('./Comment.js');
 
 //----- Users can have many posts, posts belong to one user -----
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE',
+    onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
@@ -12,4 +13,24 @@ Post.belongsTo(User, {
 });
 //---------------------------------------------------------------
 
-module.exports = { User, Post };
+//----- Comments belong to one post and one user, posts and users can have many comments -----
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+//---------------------------------------------------------------------------------------------
+
+module.exports = { User, Post, Comment };
